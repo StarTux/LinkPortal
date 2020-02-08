7364accca7534384f8a4194309b38498d2b92373
StarTux <ths.loy@googlemail.com>
2020-02-08 16:25:45 +0100

    List command

--- src/main/java/com/winthier/linkportal/LinkPortalCommand.java
+++ src/main/java/com/winthier/linkportal/LinkPortalCommand.java
@@ -1,6 +1,10 @@
 package com.winthier.linkportal;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,6 +23,36 @@ public final class LinkPortalCommand implements CommandExecutor {
             plugin.getPortals().reload();
             plugin.loadConf();
             sender.sendMessage("Link portals and config reloaded.");
+            return true;
+        } else if (args.length == 1 && args[0].equals("list")) {
+            if (player == null) return false;
+            UUID uuid = player.getUniqueId();
+            Map<String, Integer> map = new HashMap<>();
+            for (Portal portal : plugin.getPortals().getPortals()) {
+                if (!uuid.equals(portal.getOwnerUuid())) continue;
+                String name = portal.getRingName();
+                if (name == null) name = "";
+                Integer o = map.get(name);
+                int count = o != null ? o + 1 : 1;
+                map.put(name, count);
+            }
+            if (map.isEmpty()) {
+                player.sendMessage(ChatColor.RED + "You don't have any portals");
+            } else {
+                player.sendMessage(ChatColor.YELLOW + "Listing " +
+                                   (map.size() == 1
+                                    ? "1 Portal Ring:"
+                                    : map.size() + " Portal Rings:"));
+                for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                    String name = entry.getKey();
+                    int count = entry.getValue();
+                    player.sendMessage(ChatColor.AQUA + name
+                                       + ChatColor.GRAY + ": "
+                                       + (count == 1
+                                          ? "1 Portal"
+                                          : count + " Portals"));
+                }
+            }
             return true;
         }
         return true;
